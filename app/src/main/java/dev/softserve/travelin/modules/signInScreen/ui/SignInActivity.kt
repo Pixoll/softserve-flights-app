75c3a8450716473e2511b3e3bdafd209f6ff1c8c
Ricardo Cea <ricardocea.dev@gmail.com>
2025-03-31 19:42:02 -0300

    I add modules package

---
+++ app/src/main/java/dev/softserve/travelin/modules/signInScreen/ui/SignInActivity.kt
@@ -0,0 +1,31 @@
+package dev.softserve.travelin.modules.signInScreen.ui
+
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import dev.softserve.travelin.Greeting
+import dev.softserve.travelin.ui.theme.TravelinTheme
+
+class SignInActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TravelinTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
