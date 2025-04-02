ec62eb46dcf881e236b2faa15592bcfe7f57cf79
LAPTOP-GG7KVLVS\tania <taniajherrera@live.com>
2025-04-02 15:30:22 -0300

    SCRUM-22: Create splash screen UI

---
+++ app/src/main/java/dev/softserve/travelin/modules/splashScreen/ui/SplashScreenActivity.kt
@@ -0,0 +1,76 @@
+package dev.softserve.travelin.modules.splashScreen.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.softserve.travelin.R
+import dev.softserve.travelin.modules.splashScreen.ui.ui.theme.TravelinTheme
+
+class SplashScreen : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TravelinTheme {
+                    SplashScreenUI()
+                }
+            }
+        }
+    }
+
+@Composable
+fun SplashScreenUI() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF007A8C))
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = 32.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.app_logo),
+                contentDescription = "Logo",
+                modifier = Modifier
+                    .width(190.dp)
+                    .aspectRatio(234f / 80f)
+            )
+
+            Text(
+                text = "Your Passport to Seamless Adventures!",
+                color = Color.White
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SplashScreenPreview() {
+    TravelinTheme {
+        SplashScreenUI()
+    }
+}
